31dd35d235eae940392211b99faa20e0e0e689bf
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-25 11:47:04 +0530

    Update backend.tf

--- Infra/Environments/dev/backend.tf
+++ Infra/Environments/dev/backend.tf
@@ -4,6 +4,7 @@ terraform {
     key            = "eks-monitoring/staging/terraform.tfstate"
     region         = "us-east-1"            
     encrypt        = true
+    dynamodb_table = "terraform-lock-table"
   }
 }
 
